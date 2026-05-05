11209259443f5eabafae76ee9cc1f52f48fc3113
secretalyst2027 <secretalyst2027@gmail.com>
2026-05-05 22:58:13 +0700

    init

---
+++ config.ts
@@ -0,0 +1,72 @@
+import { defineConfig } from "tinacms";
+
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch: process.env.NEXT_PUBLIC_TINA_LOCAL === "true" ? "main" : branch,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "03531803-f025-4070-bb2a-d140ce3b6747",
+  token: process.env.TINA_TOKEN || "88a5501e215762d0f76c9fd0dc7a965fbf9051f0",
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "assets",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "event",
+        label: "Events / Blog",
+        path: "content/events",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "date",
+            label: "Date",
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+          },
+          {
+            type: "boolean",
+            name: "featured",
+            label: "Featured Event",
+          },
+          {
+            type: "string",
+            name: "desc",
+            label: "Description",
+            ui: {
+              component: "textarea",
+            },
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
